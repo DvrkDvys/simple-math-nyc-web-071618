aef9ac1d45372f5bc31f258bd317995d88beb78b
Jordan Harris <jharris@wework.com>
2018-07-16 21:16:39 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -24,8 +24,16 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-  i = sqrt(num)
-  return i
+  # i = sqrt(num)
+  # return i
+     return 0 if num == 0 # quick exit  
+    raise RangeError if square < 0  
+    # Actual computation  
+    n = iter(1, num)  
+    n1 = iter(n, num)  
+    n1, n = iter(n1, num), n1 until n1 >= n - 1  
+    n1 = n1 - 1 until n1*n1 <= square  
+    return n1  
 end
 
 def order_of_operation(num1, num2, num3, num4)
